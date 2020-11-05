306e98e7af1203127795ddbc6c807dc9c05bbc74
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2020-11-05 09:23:27 +0300

    Add files via upload

--- ft_even_ index _list.py
+++ ft_even_ index _list.py
@@ -1,6 +1,9 @@
 def ft_even_index_list(mass):
     a = []
-    for i in range(len(mass)):
+    l = 0
+    for i in mass:
+        l += 1
+    for i in range(l):
         if i % 2 == 0:
             a.append(mass[i])
     return a
