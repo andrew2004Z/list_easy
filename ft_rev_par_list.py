9d3666ea0d7a40fabb54e2498dffd4960fdecdb2
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2020-11-05 10:25:07 +0300

    Add files via upload

--- ft_rev_par_list.py
+++ ft_rev_par_list.py
@@ -4,15 +4,16 @@ def ft_len(mass):
         l += 1
     return l
 
+
 def ft_rev_par_list(mass):
     a = []
     if ft_len(mass) % 2 == 0:
         for i in range(1, ft_len(mass), 2):
-            a.append(mass[i])
-            a.append(mass[i - 1])
+            a += [mass[i]]
+            a += [mass[i - 1]]
     elif ft_len(mass) % 2 == 1:
         for i in range(1, ft_len(mass) - 1, 2):
-            a.append(mass[i])
-            a.append(mass[i - 1])
-        a.append(mass[-1])
+            a += [mass[i]]
+            a += [mass[i - 1]]
+        a += [mass[-1]]
     return a
