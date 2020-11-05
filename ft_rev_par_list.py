16def9fd11f2fcd9c2ece941fd9bdac4a32a9b81
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2020-11-05 09:47:47 +0300

    Add files via upload

--- ft_rev_par_list.py
+++ ft_rev_par_list.py
@@ -1,11 +1,17 @@
+def ft_len(mass):
+    l = 0
+    for i in mass:
+        l += 1
+    return l
+
 def ft_rev_par_list(mass):
     a = []
-    if len(mass) % 2 == 0:
-        for i in range(1, len(mass), 2):
+    if ft_len(mass) % 2 == 0:
+        for i in range(1, ft_len(mass), 2):
             a.append(mass[i])
             a.append(mass[i - 1])
-    elif len(mass) % 2 == 1:
-        for i in range(1, len(mass) - 1, 2):
+    elif ft_len(mass) % 2 == 1:
+        for i in range(1, ft_len(mass) - 1, 2):
             a.append(mass[i])
             a.append(mass[i - 1])
         a.append(mass[-1])
--- ft_rshift_list.py
+++ ft_rshift_list.py
@@ -1,7 +1,14 @@
+def ft_len(mass):
+    l = 0
+    for i in mass:
+        l += 1
+    return l
+
+
 def ft_rshift_list(mass):
     a = []
     p = mass[-1]
     a.append(p)
-    for i in range(len(mass) - 1):
+    for i in range(ft_len(mass) - 1):
         a.append(mass[i])
     return a
