09293f5803e29ecb161b2ab8a0b6eb7c7cb6d2c4
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2020-11-05 10:20:29 +0300

    Add files via upload

--- ft_rev_list.py
+++ ft_rev_list.py
@@ -1,2 +1,15 @@
-def ft_rev_list(mass):
-    return mass[::-1]
+def ft_len(mass):
+    l = 0
+    for i in mass:
+        l += 1
+    return l
+
+
+def ft_rev_list(num):
+    n = ft_len(num) // 2
+    for i in range(0, n):
+        a = -1 * (i + 1)
+        t = num[i]
+        num[i] = num[a]
+        num[a] = t
+    return num
