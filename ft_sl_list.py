cb5a93ff9c903c3ab99b41690b4eacfbb9f1651d
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2020-11-05 10:43:50 +0300

    Add files via upload

--- ft_sl_list.py
+++ ft_sl_list.py
@@ -1,6 +1,13 @@
+def ft_len(mass):
+    l = 0
+    for i in mass:
+        l += 1
+    return l
+
+
 def ft_sl_list(mass):
     n = 0
-    for i in range(1, len(mass)):
+    for i in range(1, ft_len(mass)):
         if mass[i - 1] < mass[i]:
             n += 1
     return n
