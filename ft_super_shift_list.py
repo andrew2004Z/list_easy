ff9fd3da944b481cd2618090d148cf18b7c21778
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2020-11-05 10:45:33 +0300

    Add files via upload

--- ft_super_shift_list.py
+++ ft_super_shift_list.py
@@ -1,3 +1,10 @@
+def ft_len(mass):
+    l = 0
+    for i in mass:
+        l += 1
+    return l
+
+
 def ft_super_shift_list(mass, n):
     if n > 0:
         mass1 = mass
@@ -5,7 +12,7 @@ def ft_super_shift_list(mass, n):
             a = []
             p = mass1[-1]
             a.append(p)
-            for i in range(len(mass1) - 1):
+            for i in range(ft_len(mass1) - 1):
                 a.append(mass1[i])
             mass1 = a
         return mass1
@@ -17,7 +24,7 @@ def ft_super_shift_list(mass, n):
         for i in range(n):
             a = []
             p = mass1[0]
-            for i in range(1, len(mass1)):
+            for i in range(1, ft_len(mass1)):
                 a.append(mass1[i])
             mass1 = a
             mass1.append(p)
